8501e62b2ed1d67573b47853b10f141a04d43fa9
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-31 21:51:14 +0000

    add folders and files for november and the questions to october final days problems

--- create_months.py
+++ create_months.py
@@ -1,20 +1,22 @@
+import calendar
 import os
 
+extensions = ['.md', '.py', '.ts']
 
-def create_november():
-    os.mkdir('11')
-    os.chdir('11')
-    for day in range(1, 31):
+
+def create_month(month, year):
+    os.mkdir(f'{month}')
+    os.chdir(f'{month}')
+    for day in range(1, calendar.monthrange(year, month)[1] + 1):
+        if day < 10:
+            day = f'0{day}'
         os.mkdir(str(day))
         os.chdir(str(day))
-        with open(f'{day}.md', 'w') as f:
-            f.write(f'# {day} November')
-        with open(f'{day}.py', 'w') as f:
-            f.write(f'# {day} November')
-        with open(f'{day}.ts', 'w') as f:
-            f.write(f'# {day} November')
+        for extension in extensions:
+            with open(f'{day}{extension}', 'w') as f:
+                f.write(f'# {day} {calendar.month_name[month]}')
         os.chdir('..')
     os.chdir('..')
 
 
-create_november()
+create_month(11, 2022)
